c8ec6dcbdf6ab598d12ed03724f05ac9113f9c1c
Purv-Sonani <purv360@gmail.com>
2025-12-14 15:32:35 +0100

    Deployment setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,8 +15,8 @@ app.use(helmet()); // Adds various HTTP headers for security
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // STRICTLY allow only your frontend
-    credentials: true, // Allow cookies (Essential for Auth)
+    origin: ["http://localhost:3000", process.env.CLIENT_URL || ""],
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
